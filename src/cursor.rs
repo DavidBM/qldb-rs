6d363cfe0de3081ad11fb657e2fb7ab8938513a1
David Bonet <david@couragium.com>
2021-01-05 19:03:48 +0100

    remove Arc & Mutex

--- src/cursor.rs
+++ src/cursor.rs
@@ -1,14 +1,11 @@
 use crate::{QLDBResult, QueryBuilder};
-use futures::lock::Mutex;
-use std::sync::Arc;
-
 use ion_binary_rs::IonValue;
 
 #[derive(Debug)]
 pub struct Cursor {
     query_builder: QueryBuilder,
     next_page: Option<String>,
-    page: Arc<Mutex<u64>>,
+    page: u64,
 }
 
 impl Cursor {
@@ -16,12 +13,12 @@ impl Cursor {
         Cursor {
             query_builder,
             next_page: None,
-            page: Arc::new(Mutex::new(0)),
+            page: 0,
         }
     }
 
     pub async fn load_more(&mut self) -> QLDBResult<Option<Vec<IonValue>>> {
-        let (values, next_page_token) = if *self.page.lock().await == 0 {
+        let (values, next_page_token) = if self.page == 0 {
             self.query_builder.execute_statement().await?
         } else if let Some(page) = &self.next_page {
             self.query_builder.get_next_page(&page).await?
