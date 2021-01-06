b7d68d11e67580ab4334b4cb5f31dddc85df595d
David Bonet <david@couragium.com>
2021-01-06 16:46:39 +0100

    document the DocumentCollection

--- src/document_collection.rs
+++ src/document_collection.rs
@@ -6,6 +6,38 @@ use ion_binary_rs::IonValue;
 use std::convert::TryFrom;
 use std::ops::Index;
 
+/// Represents a collection of documents. It implements
+/// [](std::iter::IntoIterator) so you can call
+/// [](std::iter::IntoIterator::into_iter) in order to
+/// use it in for loops or with [](std::iter::Iterator::map).
+///
+/// It implements [](std::ops::Index) too.
+///
+/// It adds some utilities methods in order to do common
+/// operations.
+/// 
+/// You can use the into_iter in order to execute aggregate 
+/// values or to make other complex operation.
+/// 
+/// ```rust,no_run
+/// 
+/// // Adds all the "points" attributes from each document.
+/// // It stops early in case of error extracting the attribute.
+/// fn count_points(matches: DocumentCollection) -> QLDBExtractResult<u64> {
+/// 
+///     // You can use other types as BigUInt, BigDecimal, etc 
+///     // in order to avoid overflow
+/// 
+///     let result: u64 = matches
+///         .into_iter()
+///         .map(|doc| doc.get_value::<u64>("points"))
+///         .collect::<Result<Vec<u64>, _>>()?
+///         .into_iter()
+///         .fold(0, |acc, val| acc + val);
+/// 
+///     Ok(result)
+/// }
+/// ```
 #[derive(Clone, Debug, PartialEq)]
 pub struct DocumentCollection {
     documents: Vec<Document>,
