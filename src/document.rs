f9fd3ebbbbabf6c739f70131efdb41d26781dc3d
sergioArgerey <sergioargerey@gmail.com>
2021-04-29 11:44:46 +0100

    cargo fmt & log remove

--- src/document.rs
+++ src/document.rs
@@ -70,10 +70,7 @@ impl Document {
 
         match T::try_from(element.clone()) {
             Ok(result) => Ok(Some(result)),
-            Err(err) => {
-                println!("{}", std::any::type_name::<T>());
-                return Err(QLDBExtractError::BadDataType(Box::new(err)));
-            },
+            Err(err) => Err(QLDBExtractError::BadDataType(Box::new(err))),
         }
     }
 }
